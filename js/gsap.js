c1e838f321f9e0aed940bd73a4b51e2a90ce3ab4
vigs950813SVGi <renovatio_bluesunset@hotmail.com>
2024-01-17 06:38:59 -0600

    Se anexa correo a primer contacto

--- js/gsap.js
+++ js/gsap.js
@@ -1,11 +1,18 @@
-gsap.registerPlugin(TextPlugin)
- 
- // Selecciona el elemento que deseas animar
- const miTexto = document.getElementById('miTexto');
+ // Registra el plugin TextPlugin
+ gsap.registerPlugin(TextPlugin);
 
-//replaces yourElement's text with "This is the new text" 
-gsap.to(miTexto, {
-    duration: 2,
-    text: "This is the new text",
-    ease: "none",
-  });
+ // Definir la función para transformar el texto
+ function transformarTexto() {
+   // Selecciona el elemento que deseas animar
+   const miTexto = document.getElementById('miTexto');
+
+   // Utiliza GSAP con TextPlugin para animar el texto
+   gsap.to(miTexto, {
+     duration: 2,
+     text: "¡Hola, soy un texto animado!",
+     ease: 'power2.out'
+   });
+ }
+
+  // Asocia la función al clic del botón con id 'btnAcordeon'
+  document.getElementById('btnAcordeon').addEventListener('click', transformarTexto);
